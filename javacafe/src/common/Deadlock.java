db8414d2d151237c845ebffd2ae96fdd132730d3
leadon2010 <leadon@empal.com>
2019-09-07 13:14:43 +0900

    kk

---
+++ javacafe/src/common/Deadlock.java
@@ -0,0 +1,47 @@
+package common;
+
+public class Deadlock {
+	static class Friend {
+		private final String name;
+
+		public Friend(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public synchronized void bow(Friend bower) {
+			System.out.format("%s: %s. " + "  has bowed to me!%n", this.name, bower.getName());
+			bower.bowBack(this);
+		}
+
+		public synchronized void bowBack(Friend bower) {
+			System.out.format("%s: %s. " + " has bowed back to me!%n", this.name, bower.getName());
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		final Friend alphonse = new Friend("Alphonse");
+		final Friend gaston = new Friend("Gaston");
+		Thread tha = new Thread(new Runnable() {
+			public void run() {
+				alphonse.bow(gaston);
+			}
+		});
+		tha.setName("AAA");
+		System.out.println(tha.getName());
+		tha.start();
+		Thread thb = new Thread(new Runnable() {
+			public void run() {
+				gaston.bow(alphonse);
+			}
+		});
+		thb.setName("BBB");
+		System.out.println(thb.getName());
+		thb.start();
+		System.out.println("end");
+	}
+}
---
+++ javacafe/src/common/My.java
@@ -0,0 +1,12 @@
+package common;
+
+public class My {
+	public String getText() {
+		return "Hello";
+	}
+
+	public static void main(String[] args) {
+		String[] str = { "a", "b" };
+		Deadlock.main(str);
+	}
+}
